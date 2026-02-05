a672624656a18325224e6ea2a811841dbe16701b
Kumar <ksitakumar2@gmail.com>
2026-02-05 10:31:29 +0530

    message

---
+++ 08.SpecVar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "all variables passed to script: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "current dir: $PWD"
+echo "Users running script: $USER"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
