101a79563528a40749c432dffaaa4a851d9abc05
Kumar <ksitakumar2@gmail.com>
2026-02-05 11:34:24 +0530

    message

--- 09.condition.sh
+++ 09.condition.sh
@@ -5,6 +5,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
     then
         echo "you must login as root user to run the script"
+        exit 1
     else
         echo "You are running as root user"
 fi
