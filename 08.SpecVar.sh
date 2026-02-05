2f1a5f5bd448c57c32127244b2cf7b89cafcb739
Kumar <ksitakumar2@gmail.com>
2026-02-05 10:33:50 +0530

    message

--- 08.SpecVar.sh
+++ 08.SpecVar.sh
@@ -5,6 +5,7 @@ echo "number of variables: $#"
 echo "script name: $0"
 echo "current dir: $PWD"
 echo "Users running script: $USER"
+echo "Users home directory: $HOME"
 echo "PID of the script: $$"
 sleep 10 &
 echo "PID of last command in background: $!"
