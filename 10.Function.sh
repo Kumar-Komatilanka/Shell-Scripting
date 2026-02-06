761ec789edfb3b6b9a6005dec3b9b2ac597abcc7
Kumar <ksitakumar2@gmail.com>
2026-02-06 10:24:59 +0530

    hello world implemented

---
+++ 10.Function.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+echo " You must be a root user to install the programme"
+exit 1
+else 
+echo "You are running as a root user"
+fi
+VALIDATE(){
+if [ &1 -eq 0 ]
+echo "$2 was installed successfully"
+else
+echo " $2 was not installed successfully"
+exit 1
+fi
+}
+dnf installed Mysql
+if [ $? -ne 0 ]
+then
+echo "Mysql is not installed.. Going to install"
+dnf install Mysql -y
+VALIDATE $? "Mysql"
+else
+echo "progrmme is already installed, nothing to do"
+fi
+if [ $? -ne 0 ]
+then
+echo "nginx is not installed.. Going to install"
+dnf install nginx -y
+VALIDATE $? "nginx"
+else
+echo "nginx is already installed, nothing to do"
+fi
+
