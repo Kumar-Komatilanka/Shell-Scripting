6cba5e7eb441abf095bc25312766d6843cf74756
Kumar <ksitakumar2@gmail.com>
2026-02-14 16:24:57 +0530

    calling scripts

--- script1.sh
+++ script1.sh
@@ -2,6 +2,7 @@
 Greeting=Kumar
 echo "Welcome to my world"
 echo "PID of script1.sh is $$"
+echo "My village is $Home"
 
 #./script2.sh
 source ./script2.sh
--- script2.sh
+++ script2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+Home=Mukkamala
 echo "Welcome to my home"
 echo "process id of script2sh is $$"
 
