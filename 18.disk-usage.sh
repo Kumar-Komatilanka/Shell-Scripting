ae54ce2cd050d63164122faf7efcaae2758b905b
Kumar <ksitakumar2@gmail.com>
2026-03-13 11:57:38 +0530

    While loop condition

--- 18.disk-usage.sh
+++ 18.disk-usage.sh
@@ -4,7 +4,7 @@ DISK_THRESOLD=1 #in project it will be 75
 
 while IFS= read line
 do 
-USAGE=$(echo $line | awk 'print {$6}' | cut -d "%" -f1)
-PARTITION=$(echo $line | awk 'print {$7}')
+USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+PARTITION=$(echo $line | awk '{print $7}')
 echo "$PARTITION $USAGE"
 done <<< $DISK_USAGE
