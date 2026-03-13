97cf3cf7f8fa105068c7ff17987d6cbbc0d55702
Kumar <ksitakumar2@gmail.com>
2026-03-13 11:56:54 +0530

    While loop condition

--- 18.disk-usage.sh
+++ 18.disk-usage.sh
@@ -2,7 +2,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESOLD=1 #in project it will be 75
 
-while IFS=read line
+while IFS= read line
 do 
 USAGE=$(echo $line | awk 'print {$6}' | cut -d "%" -f1)
 PARTITION=$(echo $line | awk 'print {$7}')
